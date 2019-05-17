95e21d5dffb5243946a4651da2ae6c5efa424f12
kwokmoonho <kwokmoonho@gmail.com>
2019-05-17 10:34:10 -0600

    added second line of code

--- src/kalugrace95/Main.java
+++ src/kalugrace95/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("My name is Grace");
+        System.out.println("HI, This is Wilson");
     }
 }
